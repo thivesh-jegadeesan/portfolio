57cada8846a37701793f635e2a9cb06ade236e11
thivesh-jegadeesan <151930863+thivesh-jegadeesan@users.noreply.github.com>
2023-12-28 17:00:57 +0000

    url changed

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.allapp),
+    path("all", views.allapp),
     path("Home", views.home),
     path("about", views.about),
     path("resume", views.resume),
