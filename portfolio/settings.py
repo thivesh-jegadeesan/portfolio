f6d118a0dc9e6f622ee9303208d774c580d72121
thivesh-jegadeesan <151930863+thivesh-jegadeesan@users.noreply.github.com>
2023-12-30 20:36:43 +0000

    static file movement

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -129,7 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'home', 'static'),  # Adjust this path based on your project structure.
+],
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_build', 'static '),
 
 
