85b7373fce48a8d9cffc58d4674ab58356f031d0
thivesh-jegadeesan <151930863+thivesh-jegadeesan@users.noreply.github.com>
2023-12-30 20:22:42 +0000

    postgress convertion

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio',
+        'USER': 'thiveshj',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',   # Set to the PostgreSQL server address
+        'PORT': '5432',        # Set to the PostgreSQL server port
     }
 }
 
